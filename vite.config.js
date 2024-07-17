7d206b5777867dd36ae5e24d6422d3fc26c355c8
grommoott <grommoott@gmail.com>
2024-07-17 14:44:34 +0700

    snapshot-0.5-5

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
     server: {
         proxy: {
-            "/api": "https://localhost:10000",
+            "/api": "https://localhost:4000",
         },
     },
     plugins: [react()],
