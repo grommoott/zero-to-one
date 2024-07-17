f3d1117590714f5277237347a1b7c9b0d14c50b0
grommoott <grommoott@gmail.com>
2024-07-17 14:24:21 +0700

    snapshot-0.5-2

--- server/index.js
+++ server/index.js
@@ -7,13 +7,8 @@ const PORT = process.env.PORT || 7777
 const app = express()
 
 app.use(cors())
-app.use(express.static("../dist"))
 app.use(express.json())
 app.use("/", router)
-
-app.get("/", (_, res) => {
-    res.sendFile("index.html")
-})
 
 app.listen(PORT, () => {
     console.log("Server started on port " + PORT)
